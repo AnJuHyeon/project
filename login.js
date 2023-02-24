34c44e2c6c35a120e3a21fd57b7512a978cc1758
AnJuHyun <anju3802@naver.com>
2023-02-24 20:25:47 +0900

    no message

---
+++ login.js
@@ -0,0 +1,14 @@
+var loginId = document.querySelector("#input-log_id")
+var loginPw = document.querySelector("#input-log_pw")
+var loginBtn = document.querySelector("#log-btn")
+
+    join.disabled = true;
+
+longinId.onkeyup = function(){
+
+}
+
+
+loginId.addEventListener("keyup", );
+loginPw.addEventListener("keyup", );
+
