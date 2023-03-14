1fa8d32e1163c4af44e1bebf64304c9b194b94fd
AnJuHyun <anju3802@naver.com>
2023-03-14 20:04:58 +0900

    feildset 다 없앰.  다른 곳은 약간씩 수정

--- login.js
+++ login.js
@@ -1,14 +1,1 @@
-var loginId = document.querySelector("#input-log_id")
-var loginPw = document.querySelector("#input-log_pw")
-var loginBtn = document.querySelector("#log-btn")
-
-    join.disabled = true;
-
-longinId.onkeyup = function(){
-
-}
-
-
-loginId.addEventListener("keyup", );
-loginPw.addEventListener("keyup", );
 
