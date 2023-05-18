30327cf5b2ad57677e4370a1f863285910e3d5a3
AnJuHyun <anju3802@naver.com>
2023-05-18 11:58:21 +0900

    모달창 추가, 파일 올리면  사진 모아두는 곳으로 이동하기 구현

--- seeReview/dessert_1.js
+++ seeReview/dessert_1.js
@@ -24,52 +24,59 @@ marker.setMap(map);
 // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
 // marker.setMap(null);    
 
-function onPopUp(){
-    window.open("dessert_1pics.html", "new",'width=600,height=600 left=400, top=80')
-}
 
-
-
-document.getElementById("image").addEventListener("change", function(event){
-  const imageFiles = event.target.files;
-  const previewDiv = document.getElementById("preview");
+document.getElementById("image").addEventListener("change", function(event){  //change 는 변동이 있을 때 발생
+  const imageFiles = event.target.files; //이벤트가 발생한 대상 객체
+  const previewBox = document.getElementById("preview");
 
   for(let i = 0; i < imageFiles.length; i++){
-     const reader = new FileReader();
+     const reader = new FileReader(); 
 
      reader.onload = function(event){
-        const imageSrc = event.target.result;
-        const imageElement = document.createElement("img");
-        imageElement.style.width = "22vh";
-        imageElement.style.height = "22vh";
-        imageElement.src = imageSrc;
-        imageElement.classList.add("preview-image");
+      const imageSrc = event.target.result; //이벤트가 발생한 대상 객체를 가리킴 정확히 잘 모르겠음
+        const imageElement = document.createElement("img"); //요소 추가
+        imageElement.style.width = "22vh"; //이미지 크기 조절
+        imageElement.style.height = "22vh";//이미지 크기 조절
+        imageElement.src = imageSrc; //이미지 소스의 URL 명시
+        imageElement.classList.add("preview-image");//class값 추가
 
-        previewDiv.appendChild(imageElement);
+        previewBox.appendChild(imageElement);//특정 부모 노드의 자식노드 리스트 중 마지막 자식으로 붙임.
      };
 
      reader.readAsDataURL(imageFiles[i]);
   }
 });
 
-document.getElementById("submit").addEventListener("click", function(){
-   const previewDiv = document.getElementById("preview");
+document.getElementById("submit").addEventListener("click", function(){ //submit 버튼을 눌렀을 때
+   const previewBox = document.getElementById("preview");
    const resultUl = document.getElementById("upload");
 
-   const previewImages = previewDiv.querySelectorAll(".preview-image");
+   const previewImages = previewBox.querySelectorAll(".preview-image"); //미리보기 사진 전부
    for(let i = 0; i < previewImages.length; i++){
       const imageElement = previewImages[i];
-      imageElement.style.width = "22vh";
-      imageElement.style.height = "22vh";
+      imageElement.style.width = "22vh"; //이미지 크기 조절
+      imageElement.style.height = "22vh";//이미지 크기 조절
 
       const listItem = document.createElement("li");
-      listItem.style.float = "left"; // 이미지를 왼쪽에 정렬하기 위해 float 속성 설정
-      listItem.appendChild(imageElement);
-      resultUl.insertBefore(listItem, resultUl.firstChild); // 첫 번째 자식으로 삽입
+      listItem.appendChild(imageElement); 
+      resultUl.insertBefore(listItem, resultUl.firstChild); //참조된 노드 앞에 특정 부모 노드의 자식 노드를 삽입 
+      //listItem이 노드 리스트의 끝에 삽입된다.
+
    }
 
-   // 미리보기 영역 비우기
-   previewDiv.innerHTML = "";
+   previewBox.innerHTML = ""; //미리보기 공간 비우기
 
   
 });
+
+
+const open = function(){   //open button 눌렀을 때 함수
+    document.querySelector(".modal").classList.remove("hidden");
+}
+const close = function(){  //close button 눌렀을 때 함수
+    document.querySelector(".modal").classList.add("hidden");
+}
+
+document.querySelector(".openBtn").addEventListener("click", open);//눌렀을 때 이벤트
+document.querySelector(".closeBtn").addEventListener("click", close);//눌렀을 때 이벤트
+document.querySelector(".bg").addEventListener("click", close);//눌렀을 때 이벤트
